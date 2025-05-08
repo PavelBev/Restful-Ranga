e30e762dc478f13ac5b502c26e0c6aa83ff9b496
Биксалиев Павел <pavelbev@gmail.com>
2025-05-08 12:52:31 +0300

    Add .gitignore

--- currency-exchange-service/target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-insert into currency_exchange
-(id, currency_from, currency_to, conversion_multiple, environment)
-values(10001, 'USD', 'RUB', 65, '');
-insert into currency_exchange
-(id, currency_from, currency_to, conversion_multiple, environment)
-values(10002, 'EUR', 'RUB', 100, '');
-insert into currency_exchange
-(id, currency_from, currency_to, conversion_multiple, environment)
-values(10003, 'AUD', 'RUB', 75, '');
